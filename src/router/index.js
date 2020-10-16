7b8e6d3d36fe6de8946759268792da31fb1badc9
fszaisl <fanshizhou@126.com>
2020-10-16 18:13:46 +0800

    sj123

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,68 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
+import LoginPage from '../views/LoginPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
     // 
-    {
-        path: '/independent',
-        name: 'independent',
-        component: Home
-    },
+    // {
+    //     path: '/independent',
+    //     component: Home,
+    //     children:[
+    //         {
+    //             path: '/independent',
+    //             component: Home,
+    //             children:[
+    //             ]
+    //         },
+    //     ]
+    // },
     {
         path: '/home',
-        name: 'home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+        component: Home,
+        children: [
+            //
+            // {
+            //     path: '/home',
+            //     component: Home,
+            // },
+            // {
+            //     path: '/independent',
+            //     component: Home,
+            // },
+        ]
+    },
+    // {
+    //     path: '/home',
+    //     name: 'home',
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: Home
+    // },
+    // {
+    //     path: '/about',
+    //     name: 'about',
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () =>
+    //         import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    // },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginPage
     },
     {
         path: '/*',
-        name: 'login',
-        component: Login
+        redirect: '/login'
     },
 
 ]
 
-const router = new VueRouter({
-    routes
-})
+const router = new VueRouter({ routes });
 
 export default router
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="home">{{message}}</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+	name: "login-page",
+	components: {},
+	beforeCreate() {
+		console.log(`beforeCreate`);
+	},
+	created() {
+		// this.axios.get('/')
+	},
+	data() {
+		return {
+			message: "这是一个login页面",
+		};
+	},
+	methods: {
+		handleClick: (evt) => {
+			console.log(evt);
+		},
+	},
+};
+</script>
